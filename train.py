44d1d9bb402602bd3ac896638f76381bb9e9cf4c
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2016-09-19 21:26:45 +0200

    Update train.py

--- train.py
+++ train.py
@@ -16,8 +16,8 @@ for dirname, firnmaes, filenames in os.walk(path):
     for filename in sorted(filenames):
         if filename.endswith(suffix):
             if first not in filename:
-                call('somoclu -k 2 -x 25 -y 15 -c ' + previous + out_suffix + ' -l 0.05 -e 3 -t exponential -r 5 ' + filename + ' ' + filename + this_suffix, shell=True)
+                call('somoclu -k 2 -x 25 -y 15 -c ' + previous + out_suffix + ' -l 0.05 -e 3 -t exponential -m toroid -r 5 ' + filename + ' ' + filename + this_suffix, shell=True)
                 # print 'somoclu -k 2 -x 25 -y 15 -c ' + previous + out_suffix + ' -l 0.05 ' + filename + ' ' + filename + this_suffix
             else:
-                call('somoclu -k 2 -x 25 -y 15 -c ' + initialcb + ' -l 0.05 -e 3 -t exponential -r 5 ' + filename + ' ' + filename + this_suffix, shell=True)
+                call('somoclu -k 2 -x 25 -y 15 -c ' + initialcb + ' -l 0.05 -e 3 -t exponential -m toroid -r 5 ' + filename + ' ' + filename + this_suffix, shell=True)
             previous = filename[0:10]
